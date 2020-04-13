195a09a0d2ba34066e32f064aeea7feb69c0be2e
Gioi Tran <ntran2@uoregon.edu>
2020-04-12 23:16:00 -0700

    annotate, exception cluster 23

--- scripts/isl1_sib_counts_10X.R
+++ scripts/isl1_sib_counts_10X.R
@@ -278,8 +278,30 @@ Top10GeneCellIdentity <- function(marker.list, df){
   feature.plot <- FeaturePlot(homeo.isl1_sib_10X, features = gene.list, label = TRUE)
   vln.plt <- VlnPlot(homeo.isl1_sib_10X, features = gene.list, pt.size = 0)
   return(list(top10.df, feature.plot, vln.plt))
+  dev.off()
   }
 Top10GeneCellIdentity(marker.list = ap.cells.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.ap.cell.clusters <- c("7", "15", "5")
+Top10GeneCellIdentity(marker.list = polar.cells.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.polar.cell.cluster <- c("14", "6", "9", "3")
+Top10GeneCellIdentity(marker.list = hair.cells.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.hair.cell.cluster <- c("8", "10", "13", "16", "17", "18")
+Top10GeneCellIdentity(marker.list = mantle.cells.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.mantle.cell.cluster <- c("5", "2", "19","21", "22","24","0","3")
+Top10GeneCellIdentity(marker.list = ring.cell.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.ring.cell.cluster <- c("20","7","24")
+Top10GeneCellIdentity(marker.list = central.cell.markers, df = all.markers.homeo.isl1_sib_10X)
+rename.central.cell.cluster <- c("1","11","4","19","12")
+
+####Rename Clusters####
+for (x in rename.ap.cell.clusters){
+  
+  homeo.isl1_sib_10X <- RenameIdents(homeo.isl1_sib_10X, x = "A-P Cells")
+  print(x)
+}
+homeo.isl1_sib_10X <- RenameIdents(homeo.isl1_sib_10X, "5" = "A-P Cells")
+DimPlot(homeo.isl1_sib_10X,reduction = "umap",
+        label = TRUE, pt.size= 0.4, repel = TRUE)
 
 ####FeaturePlotToPNG####
 figure_dir <- "isl1_sib_counts_10X_figures"
@@ -288,7 +310,6 @@ FeaturePlotToPng <- function(marker.list, dir_name) {
     #to.png <- FeaturePlot(homeo.isl1_sib_10X, features = x, label = TRUE) 
     print(x)
     mypath <- file.path("./", figure_dir, paste(x, ".png", sep = ""))
-    #print(Filing )
     print(mypath)
     
     png(file=mypath,width = 11, height = 9, units = 'in', res = 300)
@@ -304,6 +325,27 @@ FeaturePlotToPng(mantle.cells.markers, dir_name = figure_dir)
 FeaturePlotToPng(ring.cell.markers, dir_name = figure_dir)
 FeaturePlotToPng(central.cell.markers, dir_name = figure_dir)
 
+####VlnPlotToPNG####
+#figure_dir <- "isl1_sib_counts_10X_figures"
+VlnPlotToPng <- function(marker.list, dir_name) {
+  for (x in marker.list){
+    #to.png <- FeaturePlot(homeo.isl1_sib_10X, features = x, label = TRUE) 
+    print(x)
+    mypath <- file.path("./", figure_dir, paste(x, "_vlnplot.png", sep = ""))
+    print(mypath)
+    
+    png(file=mypath,width = 11, height = 9, units = 'in', res = 300)
+    print(VlnPlot(homeo.isl1_sib_10X, features = x, , pt.size = 0))
+    dev.off()
+  }
+}
+
+VlnPlotToPng(ap.cells.markers, dir_name = figure_dir)
+VlnPlotToPng(polar.cells.markers, dir_name = figure_dir)
+VlnPlotToPng(hair.cells.markers, dir_name = figure_dir)
+VlnPlotToPng(mantle.cells.markers,dir_name = figure_dir)
+VlnPlotToPng(ring.cell.markers, dir_name = figure_dir)
+VlnPlotToPng(central.cell.markers, dir_name = figure_dir)
 
 
 ####Save Figures to PDF####

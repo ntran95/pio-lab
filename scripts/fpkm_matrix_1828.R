5c67bfd3faff5252d3e92799e4be9f54d56afe55
Gioi Tran <ntran2@uoregon.edu>
2020-04-14 10:30:13 -0700

    align ensembl id to gene.name.uniq in matrix

--- scripts/fpkm_matrix_1828.R
+++ scripts/fpkm_matrix_1828.R
@@ -2,3 +2,30 @@ setwd("/Volumes/easystore/SIMR_2019/pio-lab/scripts/")
 script_name <- "fpkm_matrix_1828"
 
 readRDS("../data/fpkm_matrix_1828.RDS")
+
+#reading in .rds, contains the expression matrix 
+#variable fpkm_matrix_1828 contains 32489 (features) x 649 (cells) sparse Matrix of class "dgCMatrix"
+#similarly to Read10X(data.dir = "../data/etc/")
+fpkm_matrix_1828.data <- readRDS("../data/fpkm_matrix_1828.RDS")
+
+fpkm_matrix_1828_smartseq2 <- CreateSeuratObject(counts = fpkm_matrix_1828.data, project = "fpkm_smartseq2", min.cells = 1, min.features = 1)
+
+fpkm_matrix_1828_smartseq2
+#26320 features across 649 samples within 1 assay 
+
+# convert rownames of fpkm expression matrix ()
+#use test variable first
+fpkm_expression_mtx <- readRDS("../data/fpkm_matrix_1828.RDS")
+gene_table <- read.table("../data/Danio_Features_unique_Ens98_v1.tsv", sep = "\t", header = TRUE)
+as.matrix(gene_table.m)
+row.names(gene_table) <- gene_table$Gene.stable.ID
+#load gene table
+#fpkm_expression_mtx.merge <- merge(fpkm_expression_mtx, gene_table, by.x = rownames(fpkm_expression_mtx))
+
+#matched <- cbind(fpkm_expression_mtx, gene_table[, "Gene.name.uniq"][match(rownames(fpkm_expression_mtx), rownames(gene_table))])
+
+new.mtx <- as.matrix(merge(fpkm_expression_mtx,gene_table["Gene.name.uniq"],by="row.names",all.x=TRUE))
+
+#check to see if gene symbol [column 651] matches ENSGARG ensembl id (row 1)
+new.mtx[1,c(1,651)]
+#add treatments to metadata (1hr and homeo)

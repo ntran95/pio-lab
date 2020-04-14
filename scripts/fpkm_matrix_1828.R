3890f3c2b3a0af670e5aba0237862183ed648f7f
Gioi Tran <ntran2@uoregon.edu>
2020-04-14 11:25:43 -0700

    organized

--- scripts/fpkm_matrix_1828.R
+++ scripts/fpkm_matrix_1828.R
@@ -6,38 +6,40 @@ readRDS("../data/fpkm_matrix_1828.RDS")
 #reading in .rds, contains the expression matrix 
 #variable fpkm_matrix_1828 contains 32489 (features) x 649 (cells) sparse Matrix of class "dgCMatrix"
 #similarly to Read10X(data.dir = "../data/etc/")
+#keep original matrix for reference
 fpkm_matrix_1828.data <- readRDS("../data/fpkm_matrix_1828.RDS")
 
-fpkm_matrix_1828_smartseq2 <- CreateSeuratObject(counts = fpkm_matrix_1828.data, project = "fpkm_smartseq2", min.cells = 1, min.features = 1)
 
-fpkm_matrix_1828_smartseq2
-#26320 features across 649 samples within 1 assay 
-
-# convert rownames of fpkm expression matrix ()
-#use test variable first
+###########convert rownames of fpkm expression matrix ()###########
+#load gene table
 fpkm_expression_mtx <- readRDS("../data/fpkm_matrix_1828.RDS")
 gene_table <- read.table("../data/Danio_Features_unique_Ens98_v1.tsv", sep = "\t", header = TRUE)
-as.matrix(gene_table.m)
+as.matrix(gene_table)
 row.names(gene_table) <- gene_table$Gene.stable.ID
-#load gene table
-#fpkm_expression_mtx.merge <- merge(fpkm_expression_mtx, gene_table, by.x = rownames(fpkm_expression_mtx))
-
-#matched <- cbind(fpkm_expression_mtx, gene_table[, "Gene.name.uniq"][match(rownames(fpkm_expression_mtx), rownames(gene_table))])
-
-new.mtx <- merge(fpkm_expression_mtx,gene_table["Gene.name.uniq"],by="row.names",all.x=TRUE)
+#merge
+fpkm_expression_mtx <- merge(fpkm_expression_mtx,gene_table["Gene.name.uniq"],by="row.names",all.x=TRUE)
 
 #check to see if gene symbol [column 651] matches ENSGARG ensembl id (row 1). gene symbol will be in last column
 #32489x 651
-new.mtx[1,c(1,651)]
+fpkm_expression_mtx[1,c(1,651)]
 
-new.mtx[3, c(1,651)]
+fpkm_expression_mtx[3, c(1,651)]
 
-#turn Gene.name.uniq into rownames
-new.mtx <- data.frame(new.mtx, row.names = "Gene.name.uniq")
+#turn Gene.name.uniq into rownames, automatically delete the colum
+fpkm_expression_mtx <- data.frame(fpkm_expression_mtx, row.names = "Gene.name.uniq")
 
 #delete ENSDARG column
-new.mtx$Row.names <- NULL
+fpkm_expression_mtx$Row.names <- NULL
+#turn back to matrix
+fpkm_expression_mtx <- as.matrix(fpkm_expression_mtx)
+#dim 32489   649
 
+###############Create Seurat Object###############
+
+fpkm_matrix_1828_smartseq2 <- CreateSeuratObject(counts = fpkm_expression_mtx, project = "fpkm_smartseq2", min.cells = 1, min.features = 1)
+
+fpkm_matrix_1828_smartseq2
+#26320 features across 649 samples within 1 assay 
 
 
 #add treatments to metadata (1hr and homeo)

072f5f2de092b53a6f5968adad90aa7a15bcf450
Gioi Tran <ntran2@uoregon.edu>
2020-04-20 22:54:53 -0700

    change PC

--- scripts/fpkm_matrix_1828.R
+++ scripts/fpkm_matrix_1828.R
@@ -3,7 +3,7 @@ setwd("/Volumes/easystore/SIMR_2019/pio-lab/scripts/")
 library(Seurat)
 # library(plotly)
 #install.packages("future")
-# library(future)
+library(future)
 library(dplyr)
 library(ggplot2)
 library(cowplot)
@@ -11,7 +11,19 @@ library(cowplot)
 # library(ggrepel)
 library(ggrepel)
 library(stringr)
-script_name <- "fpkm_matrix_1828"
+options(future.globals.maxSize = 5000 * 1024^2)
+
+
+if (FALSE) {
+  setwd("/Volumes/easystore/SIMR_2019/pio-lab/scripts")
+  
+  script_name <- "fpkm_matrix_1828"
+  
+  figurePath <- function(filename, format){paste0("/Volumes/easystore/SIMR_2019",
+                                                  "/pio-lab/scripts/", script_name, "_figures/", filename)}
+  
+  #devtools::load_all("/n/projects/ddiaz/Analysis/Scripts/SeuratExtensions")
+}
 
 # =========================================================== Load smartseq2 dataset
 
@@ -84,6 +96,125 @@ fpkm_matrix_1828_smartseq2@meta.data$treatment<- meta_smartseq2$treatment
 fpkm_matrix_1828_smartseq2@meta.data$data.set<- meta_smartseq2$data.set
 #check
 fpkm_matrix_1828_smartseq2@meta.data
+# =========================================================== Normalize smartseq2 =========================
+#note: normalization step for smartseq2 object is skipped, only specifically for this step
+# =========================================================== Add 10X Homeo data ==========================
+
+isl1_sib_10X.data <- Read10X(data.dir = "../data/isl1_sib_counts_10X")
+
+homeo.isl1_sib_10X <- CreateSeuratObject(counts = isl1_sib_10X.data, project = "homeo.isl1.sib.10X", min.cells = 1, min.features = 1)
+
+homeo.isl1_sib_10X[["percent.mt"]] <- PercentageFeatureSet(homeo.isl1_sib_10X, pattern = "^mt-")
+
+homeo.isl1_sib_10X <- subset(homeo.isl1_sib_10X, subset = nFeature_RNA > 200 & nFeature_RNA < 3000 & percent.mt < 10 & nCount_RNA <10000) 
+
+homeo.isl1_sib_10X <- NormalizeData(homeo.isl1_sib_10X, normalization.method = "LogNormalize", scale.factor = 10000, verbose = TRUE)
+
+homeo.isl1_sib_10X
+# =========================================================== Modify 10X metadata ===========================
+
+homeo.isl1_sib_10X@meta.data$data.set <- "homeo-10X"
+homeo.isl1_sib_10X@meta.data$treatment <- "homeo"
+
+#check
+head(homeo.isl1_sib_10X@meta.data)
+
+# =========================================================== Seurat Object List ================================
+
+split_fpkm_smartseq <- SplitObject(fpkm_matrix_1828_smartseq2, split.by = "data.set")
+seurat_obj_list <- c(homeo.isl1_sib_10X, split_fpkm_smartseq[1], split_fpkm_smartseq[2])
+#find common/shared genes among the three seurat objects (for IntegrateData())
+all_shared_genes <- lapply(seurat_obj_list, row.names) %>% Reduce(intersect, .) 
+
+all_shared_genes
+
+# =========================================================== Integration Loop ==================================
+
+dims = 1:15
+SC_transform <- FALSE
+
+if (SC_transform) {
+  for (i in 1:length(seurat_obj_list)) {
+    print("hi")
+    seurat_obj_list[[i]] <- SCTransform(
+      seurat_obj_list[[i]], verbose = FALSE)
+  }
+  seurat_obj_features <- SelectIntegrationFeatures(
+    object.list = seurat_obj_list, nfeatures = 2000)
+  seurat_obj_list <- PrepSCTIntegration(
+    object.list = seurat_obj_list, anchor.features = all_shared_genes)
+  # reference = 1 is the homeostatic dataset in obj list
+  obj_anchors <- FindIntegrationAnchors(object.list = seurat_obj_list,
+                                        anchor.features = seurat_obj_features, normalization.method = "SCT",
+                                        dims = dims, reference = 1) 
+  obj_integrated <- IntegrateData(anchorset = obj_anchors, dims = dims,
+                                  normalization.method = "SCT")
+} else {
+  for (i in 1:length(seurat_obj_list)) {
+    seurat_obj_list[[i]] <- NormalizeData(seurat_obj_list[[i]], verbose = FALSE)
+    seurat_obj_list[[i]] <- FindVariableFeatures(seurat_obj_list[[i]], selection.method = "vst",nfeatures = 2000, verbose = FALSE)
+  }
+  obj_anchors <- FindIntegrationAnchors(object.list = seurat_obj_list,
+                                        dims = dims, reference = 1) # 1 is the homeostatic dataset in obj list
+  obj_integrated <- IntegrateData(anchorset = obj_anchors, dims = dims, features.to.integrate = all_shared_genes)
+  DefaultAssay(obj_integrated) <- "integrated"
+}
+
+obj_integrated@meta.data$data.set <- factor(obj_integrated@meta.data$data.set, ordered = TRUE)
+
+# =========================================================== UMAP/Clustering
+if (!SC_transform) {
+  obj_integrated <- ScaleData(obj_integrated, verbose = FALSE)
+  obj_integrated <- RunPCA(obj_integrated, npcs = 100, verbose = TRUE, features = NULL)
+  obj_integrated <- FindNeighbors(obj_integrated, dims = 1:15)
+  obj_integrated <- FindClusters(obj_integrated, resolution = 1.2)
+  obj_integrated <- RunUMAP(obj_integrated, reduction = "pca", dims = 1:15)
+  
+}
+
+obj_integrated_by_trt_umap <- DimPlot(obj_integrated, group.by  = "data.set", pt.size = 0.4) + DarkTheme() 
+
+obj_integrated_by_trt_umap
+
+obj_integrated_unlabeled <- DimPlot(obj_integrated, label = TRUE, pt.size = 0.4) + NoLegend()
+
+obj_integrated_unlabeled
 
 
+# ===========================================================  Common Features # =========================================================== 
+common_features <- scan(paste0("/Volumes/easystore/SIMR_2019/pio-lab/data/gene-lists/common_neuromast_features.txt"), what = "character")
+
+marker.ident <- c("early-HC", "early-HC", "early-HC", "mature-HC", "early-HC",
+                  "prog-HC", "prog-HC", "prog-HC", "prog-HC", "early-HC",
+                  "prog-HC", "prog-HC", "prog-HC", "prog-HC", "prog-HC",
+                  "prog-HC", "early-HC", "D/V Pole", "D/V Pole", "Mantle",
+                  "Mantle", "Mantle", "Amplifying Support", "Amplifying Support", "Central",
+                  "Central", "Central", "Central", "Central", "Central",
+                  "AP Pole", "AP Pole", "AP Pole", "Common neuromast cell types", "Interneuromast",
+                  "Interneuromast/Mantle")
+common_features_meta <- data.frame(common_features, marker.ident)
+
+integrated_featplt <- FeaturePlot(obj_integrated, common_features,
+                                  reduction = "umap", pt.size = 0.25, combine = FALSE, label = TRUE)
+for (i in 1:length(integrated_featplt)) {
+  integrated_featplt[[i]] <- integrated_featplt[[i]] + NoLegend() + NoAxes()
+}
+png(figurePath("common_features.png"), width = 40,
+    height = 80, units = "in", res = 200)
+print(cowplot::plot_grid(plotlist = integrated_featplt, ncol = 4))
+dev.off()
+
+################################VlnPlot#########################################
+integrated_vln <- VlnPlot(obj_integrated, common_features,pt.size = 0.0, combine = FALSE, log = TRUE)
+for (i in 1:length(integrated_vln)) {
+  integrated_vln[[i]] <- integrated_vln[[i]] + NoLegend() + NoAxes()
+}
+png(figurePath("vln_common_features.png"), width = 40,
+    height = 80, units = "in", res = 200)
+print(cowplot::plot_grid(plotlist = integrated_vln, ncol = 4))
+dev.off()
+
+# ===========================================================  FindAllMarkers ===========================================
+plan("multiprocess")
+all.markers.integrated <- FindAllMarkers(obj_integrated, only.pos = FALSE, min.pct = 0.01, logfc.threshold = 0.01, return.thresh = 0.001, verbose = TRUE)
 

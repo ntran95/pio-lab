f4b7f189ef5f281ef34641a5f1d27cffe6e76686
ntran95 <ntran2@uoregon.edu>
2020-05-05 10:00:46 -0700

    post-umap clustering, automation to change DefaultAssay()

--- scripts/adj_fpkm_1828_smartseq_integ.R
+++ scripts/adj_fpkm_1828_smartseq_integ.R
@@ -13,7 +13,7 @@ if (TRUE) {
   
   setwd("/home/ntran2/bgmp/pio-lab/scripts")
   
-  script_name <- "homeo_samples_integ"
+  script_name <- "adj_fpkm_1828_smartseq_integ"
   
   figurePath <- function(filename, format){paste0(script_name, "_figures/", filename)}
   
@@ -95,22 +95,41 @@ fpkm_matrix_1828_smartseq2@meta.data
 
 homeo_samples_integ <- readRDS("../data/homeo_samples_integ.RDS")
 
+#search for NAs
+grep("TRUE",is.na(colnames(homeo_samples_integ)))
+
 # =========================================================== Seurat Object List ================================
+#split homeo data by samples (c("isl1_10X","L29314", "L34727", "L34728"))
 split_homeo_samples_integ <- SplitObject(homeo_samples_integ, split.by = "data.set")
+#split smartseq by treatment (c(1hr-smrtseq, homeo-smrtseq))
 split_fpkm_smartseq <- SplitObject(fpkm_matrix_1828_smartseq2, split.by = "data.set")
 
 seurat_obj_list <- c(split_homeo_samples_integ, split_fpkm_smartseq)
 
 all_shared_genes <- lapply(seurat_obj_list, row.names) %>% Reduce(intersect, .) 
 
-head(all_shared_genes)
+length(all_shared_genes)
 
 length(seurat_obj_list)
+
+for (i in 1:length(seurat_obj_list)){
+  #get current assay
+  #print(DefaultAssay(object = seurat_obj_list[[i]]))
+  current_assay <-DefaultAssay(object = seurat_obj_list[[i]])
+  #print(current_assay)
+  #change "integrated" assay back to "RNA" 
+  if (current_assay == "integrated"){
+    #print(current_assay)
+    DefaultAssay(seurat_obj_list[[i]]) <- "RNA"
+  }
+  print(DefaultAssay(object = seurat_obj_list[[i]]))
+}
 
 # =========================================================== Integration Loop ==================================
 
 dims = 1:15
 SC_transform <- FALSE
+
 
 if (SC_transform) {
   for (i in 1:length(seurat_obj_list)) {
@@ -130,16 +149,50 @@ if (SC_transform) {
                                   normalization.method = "SCT")
 } else {
   for (i in 1:length(seurat_obj_list)) {
-    seurat_obj_list[[i]] <- NormalizeData(seurat_obj_list[[i]], verbose = FALSE)
-    seurat_obj_list[[i]] <- FindVariableFeatures(seurat_obj_list[[i]], selection.method = "vst",nfeatures = 2000, verbose = FALSE)
+    seurat_obj_list[[i]] <- NormalizeData(seurat_obj_list[[i]], verbose = TRUE)
+    seurat_obj_list[[i]] <- FindVariableFeatures(seurat_obj_list[[i]], selection.method = "vst",nfeatures = 2000, verbose = TRUE)
   }
   obj_anchors <- FindIntegrationAnchors(object.list = seurat_obj_list,
                                         dims = dims, reference = 1) # 1 is the homeostatic dataset in obj list
   obj_integrated <- IntegrateData(anchorset = obj_anchors, dims = dims, features.to.integrate = all_shared_genes)
   DefaultAssay(obj_integrated) <- "integrated"
+  
 }
 
 obj_integrated@meta.data$data.set <- factor(obj_integrated@meta.data$data.set, ordered = TRUE)
 
+# =========================================================== UMAP/Clustering
 
+dim_list <- c(10,15,20,25,30)
 
+common_features <- scan(paste0("../data/gene-lists/common_neuromast_features.txt"), what = "character")
+
+if (!SC_transform) {
+  plan("multiprocess")
+  obj_integrated <- ScaleData(obj_integrated, verbose = TRUE, vars.to.regress = "nCount_RNA")
+  obj_integrated <- RunPCA(obj_integrated, npcs = 100, verbose = TRUE, features = NULL)
+}
+
+for (pc in dim_list){
+  obj_integrated <- FindNeighbors(obj_integrated, dims = 1:pc, verbose = TRUE)
+  obj_integrated <- FindClusters(obj_integrated, resolution = 1.0, verbose = TRUE)
+  obj_integrated <- RunUMAP(obj_integrated, reduction = "pca", dims = 1:pc, verbose = TRUE)
+  png(figurePath(paste0("umap.by.dataset.PC",pc,".png"))
+      ,width = 11, height = 9, units = "in", res = 300)
+  print(DimPlot(obj_integrated, group.by  = "data.set") + DarkTheme())
+  dev.off()
+  png(figurePath(paste0("umap.unlabelled.PC",pc,".png"))
+      ,width = 11, height = 9, units = "in", res = 300)
+  print(DimPlot(obj_integrated))
+  dev.off()
+  integrated_featplt <- FeaturePlot(obj_integrated, common_features,
+                                    reduction = "umap", pt.size = 0.25, combine = FALSE, label = TRUE)
+  for (i in 1:length(integrated_featplt)) {
+    integrated_featplt[[i]] <- integrated_featplt[[i]] + NoLegend() + NoAxes()
+  }
+  png(figurePath(paste0("common_features.PC",pc,".png")), width = 40,
+      height = 80, units = "in", res = 200)
+  print(cowplot::plot_grid(plotlist = integrated_featplt, ncol = 4))
+  dev.off()
+  
+}

53e7de6dec26c3a7089c5d6d05cc71b6db1348f6
ntran95 <ntran2@uoregon.edu>
2020-05-07 14:10:57 -0700

    remove unspecif clusters (-pos)- homeo samples

--- scripts/homeo_samples_integ.R
+++ scripts/homeo_samples_integ.R
@@ -6,6 +6,7 @@ library(cowplot)
 library(ggrepel)
 options(future.globals.maxSize = 8000 * 1024^2)
 library(RColorBrewer)
+library(stringr)
 
 
 if (TRUE) {
@@ -130,7 +131,7 @@ if (SC_transform) {
 obj_integrated@meta.data$data.set <- factor(obj_integrated@meta.data$data.set, ordered = TRUE)
 
 
-# =========================================================== Adjust object integrated metadata
+# =========================================================== Adjust object integrated metadata ===========================================================
 
 obj_integrated@meta.data$data.set <- factor(obj_integrated@meta.data$data.set, ordered = TRUE)
 
@@ -161,7 +162,6 @@ for (pc in dim_list){
   obj_integrated <- FindNeighbors(obj_integrated, dims = 1:pc, verbose = TRUE)
   obj_integrated <- FindClusters(obj_integrated, resolution = 1.0, verbose = TRUE)
   obj_integrated <- RunUMAP(obj_integrated, reduction = "pca", dims = 1:pc, verbose = TRUE)
-  DefaultAssay(obj_integrated) <- "RNA"
   png(figurePath(paste0("umap.by.dataset.PC",pc,".png"))
       ,width = 11, height = 9, units = "in", res = 300)
   print(DimPlot(obj_integrated, group.by = "data.set", cols = brewer.pal(n = 4, name = "RdBu")) + DarkTheme()) 
@@ -170,6 +170,8 @@ for (pc in dim_list){
       ,width = 11, height = 9, units = "in", res = 300)
   print(DimPlot(obj_integrated))
   dev.off()
+  DefaultAssay(obj_integrated) <- "RNA"
+  
   integrated_featplt <- FeaturePlot(obj_integrated, common_features,
                                     reduction = "umap", pt.size = 0.25, combine = FALSE, label = TRUE)
   for (i in 1:length(integrated_featplt)) {
@@ -179,6 +181,8 @@ for (pc in dim_list){
       height = 80, units = "in", res = 200)
   print(cowplot::plot_grid(plotlist = integrated_featplt, ncol = 4))
   dev.off()
+  DefaultAssay(obj_integrated) <- "integrated"
+  
   
 }
 
@@ -189,9 +193,9 @@ obj_integrated <- RunUMAP(obj_integrated, reduction = "pca", dims = 1:25, verbos
 DimPlot(obj_integrated, group.by = "data.set")
 DimPlot(obj_integrated)
 
-#saveRDS(object = obj_integrated, file = "../data/homeo_samples_integ.RDS")
+saveRDS(object = obj_integrated, file = "../data/homeo_samples_integ.RDS")
 
-#obj_integrated <- readRDS("../data/homeo_samples_integ.RDS")
+obj_integrated <- readRDS("../data/homeo_samples_integ.RDS")
 
 meta_common_features <- read.table(file = "../data/gene-lists/meta_common_features.tsv", sep = "", header = T)
 
@@ -200,21 +204,22 @@ meta_common_features <- read.table(file = "../data/gene-lists/meta_common_featur
 
 gene_table <- read.table("../data/Danio_Features_unique_Ens98_v1.tsv", sep = "\t", header = TRUE)
 
-# =========================================================== Annotate -pos clusters
+# =========================================================== Annotate -pos clusters ===========================================================
 #desired unlabelled clusters passed through 
 pos_list <- c(12,20,21,22,23,24,25)
 pos_clusters <- vector(mode = "list", length = length(pos_list))
 
-# for (x in pos_list){
-#   print(paste0("finding markers for cluster: ", x))
-#   plan("multiprocess")
-#   assign(paste0("cluster_",x), FindMarkers(obj_integrated, ident.1 = x, only.pos = FALSE, min.pct = 0.10, logfc.threshold = 0.10, verbose = TRUE))
-#   
-#   }
+ for (x in pos_list){
+   print(paste0("finding markers for cluster: ", x))
+   #plan("multiprocess")
+   assign(paste0("cluster_",x), FindMarkers(obj_integrated, ident.1 = x, only.pos = FALSE, min.pct = 0.10, logfc.threshold = 0.10, verbose = TRUE))
+   
+   }
 
 
 for (x in 1:length(pos_list)){
   print(paste0("finding markers for cluster: ", pos_list[x]))
+  plan("multiprocess")
   pos_clusters[[x]] <- FindMarkers(obj_integrated, ident.1 = pos_list[x], only.pos = FALSE, min.pct = 0.10, logfc.threshold = 0.10, verbose = TRUE)
   print(pos_clusters[[x]])
   pos_clusters[[x]]["Gene.name.uniq"] <- row.names(pos_clusters[[x]])
@@ -223,13 +228,14 @@ for (x in 1:length(pos_list)){
 
 names(pos_clusters) <- paste("cluster_", pos_list)
 
+# =========================================================== Rename Clusters ===========================================================
 
 meta <- obj_integrated@meta.data
 colnames(meta)
 cells <- list("mature-HCs" = 0, "early-HCs" = 14,  "HC-prog" = 16,
               "central-cells" = c(1,2,3,9,19), "D/V-cells" = c(10), "A/P-cells" = c(8,18),
               "amplfying support" = c(11,17), "mantle-cells" = c(5,6), "interneuromast" = c(4,7,13,15),"col1a1b-pos" = 12,
-              "clec14a-pos" = 21, "mfap4-pos" = 23, "c1qtnf5" = 24, "apoa1b-pos" = 25, "krt91-pos" = 20, "hbbe1-pos" = 22)
+              "clec14a-pos" = 21, "mfap4-pos" = 23, "c1qtnf5-pos" = 24, "apoa1b-pos" = 25, "krt91-pos" = 20, "hbbe1-pos" = 22)
 meta$cell.type.ident <- factor(rep("", nrow(meta)),
                                levels = names(cells), ordered = TRUE)
 for (i in 1:length(cells)) {
@@ -245,5 +251,19 @@ png(figurePath("annotated.clusters.png"), width = 11,
 print(umap.labeled)
 dev.off()
 
-#saveRDS(object = obj_integrated, file = "../data/homeo_samples_integ.RDS")
+saveRDS(object = obj_integrated, file = "../data/homeo_samples_integ.RDS")
 
+# =========================================================== Remove unspecific -pos clusters ===========================================================
+Ident.list <- levels(Idents(obj_integrated))
+neuromast.list <- vector(mode = "list")
+for (i in 1:length(Ident.list)) {
+  if (grepl("-pos", Ident.list[i]) == "FALSE") {
+    neuromast.list <- append(neuromast.list, Ident.list[i])
+   }
+}
+
+obj_integrated_filtered <- subset(obj_integrated, idents = neuromast.list)
+
+levels(Idents(obj_integrated_filtered))
+
+DimPlot(obj_integrated_filtered)
